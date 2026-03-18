20325b6cf89e0a8beb99cc2daf111409a48e0188
EGRrqq <egrrqqdev@gmail.com>
2026-03-18 13:14:58 +0500

    lord

--- wezterm.lua
+++ wezterm.lua
@@ -16,25 +16,25 @@ local schema = {
    },
    config_path .. "/wezterm",
    config_path .. "/nvim",
+   config_path .. "/nixos",
    config_path,
-   home_dir .. "/.nixos",
    home_dir .. "/repos",
    sessionizer.FdSearch { home_dir .. "/repos", include_submodules = true },
 }
 config.keys = {
 	  -- Keybindings for sessionizer
-   { key = "s", mods = "ALT", action = sessionizer.show(schema) },
-   { key = "m", mods = "ALT", action = history.switch_to_most_recent_workspace },
+   { key = "m", mods = "CTRL|SHIFT", action = sessionizer.show(schema) },
+   { key = "x", mods = "CTRL|SHIFT", action = history.switch_to_most_recent_workspace },
 
    -- Keybindings for pane splitting
 	{ key = '"', mods = "CTRL|SHIFT", action = w.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 	{ key = "s", mods = "CTRL|SHIFT", action = w.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 
 	-- Keybindings for resizing panes
-	{ key = "LeftArrow", mods = "CTRL|ALT", action = w.action({ AdjustPaneSize = { "Left", 10 } }) },
-	{ key = "RightArrow", mods = "CTRL|ALT", action = w.action({ AdjustPaneSize = { "Right", 10 } }) },
-	{ key = "UpArrow", mods = "CTRL|ALT", action = w.action({ AdjustPaneSize = { "Up", 10 } }) },
-	{ key = "DownArrow", mods = "CTRL|ALT", action = w.action({ AdjustPaneSize = { "Down", 10 } }) },
+	{ key = "LeftArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Left", 10 } }) },
+	{ key = "RightArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Right", 10 } }) },
+	{ key = "UpArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Up", 10 } }) },
+	{ key = "DownArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Down", 10 } }) },
 
 	-- Keybinding for closing the current pane
 	{ key = "q", mods = "CTRL|SHIFT", action = w.action({ CloseCurrentPane = { confirm = true } }) },
@@ -42,7 +42,7 @@ config.keys = {
 	{ key = "d", mods = "CTRL|SHIFT", action = w.action({ CloseCurrentTab = { confirm = true } }) },
 	-- Show the selector, using your own alphabet
 	{
-		key = "p",
+		key = "x",
 		mods = "CTRL",
 		action = w.action({ PaneSelect = { alphabet = "0123456789", mode = "SwapWithActive" } }),
 	},
