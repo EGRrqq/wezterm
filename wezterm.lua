8b52bb971c3edacba39f5ee7b0aab00ca84cf172
EGR <egrrqqdev@gmail.com>
2026-05-22 13:28:16 +0500

    skitzo

--- wezterm.lua
+++ wezterm.lua
@@ -1,40 +1,44 @@
-local w = require 'wezterm'
+local w = require("wezterm")
 local config = {}
 
 -- sessionizer
-local sessionizer = w.plugin.require "https://github.com/mikkasendke/sessionizer.wezterm"
-local history = w.plugin.require "https://github.com/mikkasendke/sessionizer-history.git" -- the most recent functionality moved to another plugin
+local sessionizer = w.plugin.require("https://github.com/mikkasendke/sessionizer.wezterm")
+local history = w.plugin.require("https://github.com/mikkasendke/sessionizer-history.git") -- the most recent functionality moved to another plugin
 
 local home_dir = w.home_dir
-local config_path = home_dir .. ("/.config")
+local config_path = home_dir .. "/.config"
 
 local schema = {
-   options = {
-      title = "My title",
-      always_fuzzy = true,
-      callback = history.Wrapper(sessionizer.DefaultCallback), -- tell history that we changed to another workspace
-   },
-   config_path .. "/wezterm",
-   config_path .. "/nvim",
-   config_path .. "/nixos",
-   config_path,
-   home_dir .. "/repos",
-   sessionizer.FdSearch { home_dir .. "/repos", include_submodules = true },
+	options = {
+		title = "My title",
+		always_fuzzy = true,
+		callback = history.Wrapper(sessionizer.DefaultCallback), -- tell history that we changed to another workspace
+	},
+	config_path .. "/wezterm",
+	config_path .. "/nvim",
+	config_path .. "/nixos",
+	config_path,
+	home_dir .. "/repos",
+	sessionizer.FdSearch({ home_dir .. "/repos", include_submodules = true }),
 }
 config.keys = {
-	  -- Keybindings for sessionizer
-   { key = "m", mods = "CTRL|SHIFT", action = sessionizer.show(schema) },
-   { key = "x", mods = "CTRL|SHIFT", action = history.switch_to_most_recent_workspace },
+	-- Keybindings for sessionizer
+	{ key = "m", mods = "CTRL|SHIFT", action = sessionizer.show(schema) },
+	{ key = "x", mods = "CTRL|SHIFT", action = history.switch_to_most_recent_workspace },
 
-   -- Keybindings for pane splitting
+	-- Keybindings for pane splitting
 	{ key = '"', mods = "CTRL|SHIFT", action = w.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 	{ key = "s", mods = "CTRL|SHIFT", action = w.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 
 	-- Keybindings for resizing panes
 	{ key = "LeftArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Left", 10 } }) },
 	{ key = "RightArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Right", 10 } }) },
-	{ key = "UpArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Up", 10 } }) },
-	{ key = "DownArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Down", 10 } }) },
+	{ key = "UpArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Up", 3 } }) },
+	{ key = "DownArrow", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Down", 3 } }) },
+	{ key = "H", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Left", 10 } }) },
+	{ key = "L", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Right", 10 } }) },
+	{ key = "K", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Up", 3 } }) },
+	{ key = "J", mods = "CTRL|SHIFT|ALT", action = w.action({ AdjustPaneSize = { "Down", 3 } }) },
 
 	-- Keybinding for closing the current pane
 	{ key = "q", mods = "CTRL|SHIFT", action = w.action({ CloseCurrentPane = { confirm = true } }) },
@@ -43,7 +47,7 @@ config.keys = {
 	-- Show the selector, using your own alphabet
 	{
 		key = "x",
-		mods = "CTRL",
+		mods = "CTRL|SHIFT",
 		action = w.action({ PaneSelect = { alphabet = "0123456789", mode = "SwapWithActive" } }),
 	},
 }
@@ -52,5 +56,12 @@ config.keys = {
 config.color_scheme = "oneLight"
 -- font
 config.font_size = 20
+
+-- Disable gray fading on unfocused panes
+config.inactive_pane_hsb = {
+	hue = 1.0,
+	saturation = 1.0,
+	brightness = 1.0,
+}
 
 return config
